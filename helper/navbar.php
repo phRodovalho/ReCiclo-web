8e16808f9fcc0df156d721099559e15aa0ba6e4a
Phelipe S. Rodovalho <59853753+phRodovalho@users.noreply.github.com>
2022-07-16 16:37:45 -0300

    Add files via upload

---
+++ helper/navbar.php
@@ -0,0 +1,27 @@
+<div class="container d-flex align-items-center justify-content-between">
+
+    <h1 class="logo"><a href="index.php">ReCiclo</a></h1>
+
+    <nav id="navbar" class="navbar">
+        <ul>
+            <li><a class="nav-link scrollto active" href="#contact">Pontos de Coleta </a></li>
+            <li><a class="nav-link scrollto" href="#about">Pedido de Coleta </a></li>
+            <li><a class="nav-link scrollto" href="#services">Calculadora de Impacto</a></li>
+            <li><a class="nav-link scrollto " href="#portfolio">Sobre nós </a></li>
+
+            <?php
+            if (isset($_SESSION['nameUser']) == true) { //se tem usuário setado
+                if ($_SESSION['userType'] == 'P') { //se usuario é padrão não habilita crud de admin
+                    echo '<li><a class="getstarted scrollto"><span class="glyphicon glyphicon-user"></span> ' . $_SESSION['nameUser'] . '</b></a></li>';
+                    echo '<li><a class="getstarted scrollto" href="logout.php"><span class="glyphicon glyphicon-log-out"></span> <b>Logout</b></a></li>';
+                } else { //usuário admin
+                    echo '<li><a class="getstarted scrollto" href="adminUser.php"><span class="glyphicon glyphicon-user"></span> ' . $_SESSION['nameUser'] . '</b></a></li>';
+                    echo '<li><a class="getstarted scrollto" href="logout.php"><span class="glyphicon glyphicon-log-out"></span> <b>Logout</b></a></li>';
+                }
+            } else echo '<li><a class="getstarted scrollto" href="login.php"><span class="glyphicon glyphicon-user"></span> Login ou Crie sua Conta </a></li>';
+            ?>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav><!-- .navbar -->
+
+</div>
---
+++ view/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include("C:/xampp/htdocs/ReCiclo/helper/head.php");
+    session_start(); ?>
+</head>
+
+<body>
+    <header id="header" class="fixed-top">
+        <?php include("C:/xampp/htdocs/ReCiclo/helper/navbar.php"); ?>
+    </header><!-- End Header -->
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+                    <h1>Cuide do nosso planeta, ReCicle!</h1>
+                    <h2>Buscamos simplificar o processo de reciclagem, busque por pontos de reciclagem em sua cidade</h2>
+                    <div class="d-flex">
+                        <a href="#about" class="btn-get-started scrollto">Quero reciclar</a>
+                        <!-- <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>!-->
+                    </div>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2 hero-img">
+                    <img src="/ReCiclo/view/assets/img/hero-img.png" class="img-fluid animated" alt="">
+                </div>
+            </div>
+        </div>
+
+    </section><!-- End Hero -->
+
+    <main id="main">
+
+        <!-- ======= Pontos de Coleta Section ======= -->
+        <section id="contact" class="contact">
+            <div class="container">
+
+                <div class="section-title">
+                    <span>Pontos de Coleta</span>
+                    <h2>Pontos de Coleta</h2>
+                    <p>Busque por pontos de Coleta de reciclaveis cadastrados em qualquer cidade do triângulo mineiro</p>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-lg-5 d-flex align-items-stretch">
+                        <div class="info">
+                            <div class="address">
+                                <i class="bi bi-geo-alt"></i>
+                                <h4>Location:</h4>
+                                <p>A108 Adam Street, New York, NY 535022</p>
+                            </div>
+
+                            <div class="email">
+                                <i class="bi bi-envelope"></i>
+                                <h4>Email:</h4>
+                                <p>info@example.com</p>
+                            </div>
+
+                            <div class="phone">
+                                <i class="bi bi-phone"></i>
+                                <h4>Call:</h4>
+                                <p>+1 5589 55488 55s</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Contact Section -->
+
+    </main>
+
+    <?php include("C:/xampp/htdocs/ReCiclo/helper/footer.php"); ?>
+
+</body>
